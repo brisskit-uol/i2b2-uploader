bc97a0141a5a6db7ef9454f8cd4b69586cb6aba9
jbl4 <jbl4@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2014-12-19 16:53:23 +0000

    sync

---
+++ i2b2/brisskit/trunk/i2b2-uploader/src/main/resources/scripts/db-tables/hive.sql
@@ -0,0 +1,18 @@
+/*========================================
+  Postgres SQL hive inserts for a project 
+  ========================================*/ 
+
+set schema 'i2b2hive';
+
+INSERT INTO i2b2hive.CRC_DB_LOOKUP (C_DOMAIN_ID, C_PROJECT_PATH, C_OWNER_ID, C_DB_FULLSCHEMA, C_DB_DATASOURCE, C_DB_SERVERTYPE, C_DB_NICENAME, C_DB_TOOLTIP, C_COMMENT, C_ENTRY_DATE, C_CHANGE_DATE, C_STATUS_CD)
+VALUES ('BRISSKIT', '/<SCHEMA_NAME>/', '@', '<SCHEMA_NAME>', 'java:/<QTProject1DS>', 'POSTGRESQL', '<SCHEMA_NAME>', null, null, now(), now(), null);
+
+INSERT INTO i2b2hive.ONT_DB_LOOKUP (C_DOMAIN_ID, C_PROJECT_PATH, C_OWNER_ID, C_DB_FULLSCHEMA, C_DB_DATASOURCE, C_DB_SERVERTYPE, C_DB_NICENAME, C_DB_TOOLTIP, C_COMMENT, C_ENTRY_DATE, C_CHANGE_DATE, C_STATUS_CD)
+VALUES ('BRISSKIT', '<SCHEMA_NAME>/', '@', 'i2b2metadata', 'java:/<OProject1DS>', 'POSTGRESQL', 'Metadata', null, null, now(), now(), null);
+
+INSERT INTO i2b2hive.WORK_DB_LOOKUP (C_DOMAIN_ID, C_PROJECT_PATH, C_OWNER_ID, C_DB_FULLSCHEMA, C_DB_DATASOURCE, C_DB_SERVERTYPE, C_DB_NICENAME, C_DB_TOOLTIP, C_COMMENT, C_ENTRY_DATE, C_CHANGE_DATE, C_STATUS_CD)
+VALUES ('BRISSKIT', '<SCHEMA_NAME>/', '@', 'i2b2workdata', 'java:/<WProject1DS>', 'POSTGRESQL', 'Workplace', null, null, now(), now(), null);
+
+INSERT INTO i2b2hive.IM_DB_LOOKUP (C_DOMAIN_ID, C_PROJECT_PATH, C_OWNER_ID, C_DB_FULLSCHEMA, C_DB_DATASOURCE, C_DB_SERVERTYPE, C_DB_NICENAME, C_DB_TOOLTIP, C_COMMENT, C_ENTRY_DATE, C_CHANGE_DATE, C_STATUS_CD)
+VALUES ('BRISSKIT', '<SCHEMA_NAME>/', '@', 'i2b2im', 'java:/<IMProject1DS>', 'POSTGRESQL', 'IM', null, null, now(), now(), null);
+
