503d42abed562f2bc5c469003499855065fab358
jbl4 <jbl4@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2014-12-22 12:15:24 +0000

    sync

--- aaa-notes/reinit.sql
+++ aaa-notes/reinit.sql
@@ -1,19 +1,22 @@
-drop schema <SCHEMA_NAME> cascade ;
-drop user <SCHEMA_NAME> ;
+drop schema project1data cascade ;
+drop user project1data ;
+drop schema project1meta cascade ;
+drop user project1meta ;
+drop schema project1im cascade ;
+drop user project1im ;
+drop schema project1work cascade ;
+drop user project1work ;
 
--- ===========================================================
+set schema 'i2b2metadata';
+drop table project1 ;
+
 set schema 'i2b2hive';
--- ===========================================================
+delete from i2b2hive.crc_db_lookup where c_project_path = '/project1/' ;
+delete from i2b2hive.ONT_DB_LOOKUP where c_project_path = 'project1/' ;
+delete from i2b2hive.WORK_DB_LOOKUP where c_project_path = 'project1/' ;
+delete from i2b2hive.IM_DB_LOOKUP where c_project_path = 'project1/' ;
 
-delete from i2b2hive.CRC_DB_LOOKUP where C_DB_FULLSCHEMA = '<SCHEMA_NAME>' ;
-delete from i2b2hive.crc_db_lookup where c_project_path = '/<SCHEMA_NAME>/' ;
-delete from i2b2hive.ONT_DB_LOOKUP where C_DB_FULLSCHEMA = '<SCHEMA_NAME>' ;
-delete from i2b2hive.WORK_DB_LOOKUP where C_DB_FULLSCHEMA = '<SCHEMA_NAME>' ;
-delete from i2b2hive.IM_DB_LOOKUP where C_DB_FULLSCHEMA = '<SCHEMA_NAME>' ;
-
--- ===========================================================
 set schema 'i2b2pm';
--- ===========================================================
 delete from i2b2pm.PM_HIVE_DATA where domain_id = 'BRISSKIT' ;
-DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = '<SCHEMA_NAME>' ;
-delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = '<SCHEMA_NAME>' ;
+DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = 'project1' ;
+delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = 'project1' ;
