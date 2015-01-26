816ca264503a78e4a7fa816ec8de43de9075b0b7
jbl4 <jbl4@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2015-01-26 19:28:06 +0000

    Refinements to cure problem with breakdowns

--- i2b2/brisskit/trunk/i2b2-uploader/aaa-notes/reinit.sql
+++ i2b2/brisskit/trunk/i2b2-uploader/aaa-notes/reinit.sql
@@ -41,3 +41,25 @@ set schema 'i2b2pm';
 delete from i2b2pm.PM_HIVE_DATA where domain_id = 'BRISSKIT' ;
 DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = 'laheart' ;
 delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = 'laheart' ;
+
+
+/* reinitialization sql for a project with id "gpcut" 
+   NB: Will delete everything from PM where domain_id = 'BRISSKIT' */
+
+drop schema gpcut cascade ;
+drop user gpcut ;
+
+
+set schema 'i2b2metadata';
+drop table gpcut ;
+
+set schema 'i2b2hive';
+delete from i2b2hive.crc_db_lookup where c_project_path = '/gpcut/' ;
+delete from i2b2hive.ONT_DB_LOOKUP where c_project_path = 'gpcut/' ;
+delete from i2b2hive.WORK_DB_LOOKUP where c_project_path = 'gpcut/' ;
+delete from i2b2hive.IM_DB_LOOKUP where c_project_path = 'gpcut/' ;
+
+set schema 'i2b2pm';
+delete from i2b2pm.PM_HIVE_DATA where domain_id = 'BRISSKIT' ;
+DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = 'gpcut' ;
+delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = 'gpcut' ;
