6fd4d15fa9bfe303c65b24fb79d7d1a12fe76b0b
jbl4 <jbl4@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2015-01-28 13:39:52 +0000

    Ability to delete a project and all of its data (!)

--- i2b2/brisskit/trunk/i2b2-uploader/aaa-notes/reinit.sql
+++ i2b2/brisskit/trunk/i2b2-uploader/aaa-notes/reinit.sql
@@ -63,3 +63,47 @@ set schema 'i2b2pm';
 delete from i2b2pm.PM_HIVE_DATA where domain_id = 'BRISSKIT' ;
 DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = 'gpcut' ;
 delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = 'gpcut' ;
+
+
+/* generalized reinitialization sql for a project with id "gpcut" 
+   NB: Will delete everything from PM where domain_id = 'BRISSKIT' */
+
+drop schema gpcut cascade ;
+drop user gpcut ;
+
+set schema 'i2b2hive';
+delete from i2b2hive.crc_db_lookup where c_project_path = '/gpcut/' ;
+delete from i2b2hive.ONT_DB_LOOKUP where c_project_path = 'gpcut/' ;
+delete from i2b2hive.WORK_DB_LOOKUP where c_project_path = 'gpcut/' ;
+delete from i2b2hive.IM_DB_LOOKUP where c_project_path = 'gpcut/' ;
+
+set schema 'i2b2pm';
+DELETE FROM i2b2pm.PM_PROJECT_DATA where PROJECT_ID = 'gpcut' ;
+delete from i2b2pm.PM_PROJECT_USER_ROLES where PROJECT_ID = 'gpcut' ;
+
+=============================================================================
+
+
+INSERT INTO person (person_id, name)
+SELECT 1, 'Me'
+WHERE NOT EXISTS (SELECT 1 FROM person WHERE person_id = 1);
+
+INSERT INTO i2b2pm.PM_HIVE_DATA(domain_id, helpurl, domain_name, environment_cd, active, change_date, entry_date, changeby_char, status_cd)
+(
+   SELECT 'BRISSKIT', 'www.brisskit.le.ac.uk', 'BRISSKIT', 'DEVELOPMENT', 1, now(), now(), '', 'A' 
+   WHERE NOT EXISTS( SELECT * FROM I2B2PM.PM_HIVE_DATA WHERE DOMAIN_ID = 'BRISSKIT' ) 
+) ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
