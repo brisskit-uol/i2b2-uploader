4ae8351669bd9ba37454ee126d039ff582eab98c
jbl4 <jbl4@45326ef8-dd20-11e0-a5b2-39e0d6486610>
2015-02-18 17:29:59 +0000

    Crude merge of branch i2b2-uploader-jl-129 to trunk by overwriting trunk after tagging.

---
+++ i2b2/brisskit/trunk/i2b2-uploader/src/main/java/org/brisskit/i2b2/Base.java
@@ -0,0 +1,162 @@
+package org.brisskit.i2b2 ;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import org.apache.log4j.* ;
+
+public class Base {
+	
+	private static Logger logger = Logger.getLogger( Base.class ) ;
+	
+	//
+	// These are the keys (or partial keys) to properties held within the config.properties file...	
+	final static String ENVIRONMENT = "env" ;
+	
+	final static String PG_DB_NAME = "pg_db_name" ;
+	final static String PG_DB_URL = "pg_db_url" ;
+	final static String PG_DB_U = "pg_db_u";
+	final static String PG_DB_P = "pg_db_p";		
+	final static String SCRIPTS_LOCATION = "scripts_location";
+	final static String JBOSS_DEPLOYMENT_DIRECTORY = "jboss_deploy_dir";
+	final static String JBOSS_DATASET_DEFINITION_TEMPLATE = "jboss_dsfile_template" ;
+			
+	/* Properties */
+	static Properties props = new Properties();
+	protected static String pg_db_name;
+	protected static String pg_db_url;	
+	protected static String jboss_deploy_dir ;
+	protected static String pg_db_u;
+	protected static String pg_db_p;
+	
+	public static Connection con;
+	
+	static public void setUp() throws UploaderException {
+		enterTrace( "Base.setUp()" ) ; 
+		InputStream configPropertiesStream = null ;
+		try {
+			
+			if( jboss_deploy_dir == null ) {
+				
+				configPropertiesStream = Base.class.getClassLoader().getResourceAsStream( "uploader.properties" ) ;			
+				props.load( configPropertiesStream ) ;
+
+				String env = props.getProperty( ENVIRONMENT );
+
+				pg_db_name = props.getProperty( env + "." + PG_DB_NAME );
+				pg_db_url  = props.getProperty( env + "." + PG_DB_URL );
+				pg_db_u = props.getProperty( env + "." + PG_DB_U );
+				pg_db_p = props.getProperty( env + "." + PG_DB_P );
+				jboss_deploy_dir = props.getProperty( env + "." + JBOSS_DEPLOYMENT_DIRECTORY );
+
+				logger.info(env + "." + PG_DB_NAME + "= " + pg_db_name);				;
+				logger.info(env + "." + PG_DB_U + "= " + pg_db_u);
+				logger.info(env + "." + PG_DB_P + "= " + pg_db_p);	
+				logger.info(env + "." + PG_DB_URL + "= " + pg_db_url);
+				logger.info(env + "." + JBOSS_DEPLOYMENT_DIRECTORY + "= " + jboss_deploy_dir);
+
+				try { 
+					configPropertiesStream.close() ; 
+				}
+				catch( IOException iox ) {
+					logger.warn( "Could not close configPropertiesStream", iox ) ;
+				}
+			}
+			
+		} catch (IOException e) {
+			throw new UploaderException( e ) ;
+		}
+		finally {
+			exitTrace( "Base.setUp()" ) ; 
+		}
+		
+	}
+	
+	
+	static public Connection _getSimpleConnectionPG() throws UploaderException {
+		enterTrace( "getSimpleConnectionPG()" ) ;
+		
+		String DB_CONN_STRING = "jdbc:postgresql://" + pg_db_url + "/"+ pg_db_name +"?user=" + pg_db_u+ "&password=" + pg_db_p;
+		String DRIVER_CLASS_NAME = "org.postgresql.Driver";
+		String USER_NAME = pg_db_u;
+		String PASSWORD = pg_db_p;
+
+		try {
+			Class.forName(DRIVER_CLASS_NAME).newInstance();
+		} 
+		catch( Exception ex ) {
+			logger.error( "Check classpath. Cannot load db driver: " + DRIVER_CLASS_NAME ) ;
+			throw new UploaderException( ex ) ;
+		}
+
+		if (con == null) {
+			try {
+				con = DriverManager.getConnection( DB_CONN_STRING, USER_NAME, PASSWORD ) ;
+			} 
+			catch (SQLException sqlex) {
+				logger.error( "Driver loaded, but cannot connect to db: " + DB_CONN_STRING ) ;
+				throw new UploaderException( sqlex ) ;
+			}
+		}
+		exitTrace( "getSimpleConnectionPG()" ) ;
+		return con;
+	}
+	
+	static public Connection getSimpleConnectionPG() throws UploaderException {
+		enterTrace( "getSimpleConnectionPG()" ) ;
+		try {
+			if (con == null) {
+				
+				Class.forName( "org.postgresql.Driver" ).newInstance();
+				
+				con = DriverManager.getConnection( "jdbc:postgresql://" + pg_db_url + "/"+ pg_db_name +"?user=" + pg_db_u+ "&password=" + pg_db_p 
+						                         , pg_db_name
+						                         , pg_db_p ) ;
+			}
+			return con;
+		}
+		catch( InstantiationException iex ) {
+			logger.error( "Cannot load db driver: " + "org.postgresql.Driver", iex ) ;
+			throw new UploaderException( iex ) ;
+		}
+		catch( IllegalAccessException iax ) {
+			logger.error( "Cannot load db driver: " + "org.postgresql.Driver", iax ) ;
+			throw new UploaderException( iax ) ;
+		}
+		catch( ClassNotFoundException cnfex ) {
+			logger.error( "Cannot load db driver: " + "org.postgresql.Driver", cnfex ) ;
+			throw new UploaderException( cnfex ) ;
+		}
+		catch( SQLException sqlex ) {
+			logger.error( "Driver loaded, but cannot connect to db: " + "jdbc:postgresql://" + pg_db_url + "/"+ pg_db_name +"?user=" + pg_db_u+ "&password=" + pg_db_p ) ;
+			throw new UploaderException( sqlex ) ;
+		}
+		finally {
+			exitTrace( "getSimpleConnectionPG()" ) ;
+		}	
+	}
+	
+	/**
+	 * Utility routine to enter a structured message in the trace log that the given method 
+	 * has been entered. 
+	 * 
+	 * @param entry: the name of the method entered
+	 */
+	public static void enterTrace( String entry ) {
+		I2B2Project.enterTrace( logger, entry ) ;
+	}
+
+    /**
+     * Utility routine to enter a structured message in the trace log that the given method 
+	 * has been exited. 
+	 * 
+     * @param entry: the name of the method exited
+     */
+    public static void exitTrace( String entry ) {
+    	I2B2Project.exitTrace( logger, entry ) ;
+	}
+}
---
+++ i2b2/brisskit/trunk/i2b2-uploader/src/main/java/org/brisskit/i2b2/PatientMapping.java
@@ -0,0 +1,183 @@
+/**
+ * 
+ */
+package org.brisskit.i2b2;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.* ;
+
+/**
+ * @author jeff
+ *
+ */
+public class PatientMapping {
+
+	private static Logger logger = Logger.getLogger( PatientMapping.class ) ;
+	
+	public static final String PATIENT_MAP_INSERT_COMMAND = 
+			"SET SCHEMA '<DB_SCHEMA_NAME>';" +
+			"" +
+			"INSERT INTO <DB_SCHEMA_NAME>.PATIENT_MAPPING" +
+			    "( PATIENT_IDE" + 			//  VARCHAR(200)  NOT NULL,
+			    ", PATIENT_IDE_SOURCE" + 	//	VARCHAR(50)  NOT NULL,
+			    ", PATIENT_IDE_STATUS" + 	//	VARCHAR(50) NULL,
+			    ", PROJECT_ID" + 			//  VARCHAR(50) NOT NULL,
+			    ", UPLOAD_DATE" + 			//  TIMESTAMP NULL,
+			    ", UPDATE_DATE" + 			//  TIMESTAMP NULL,
+			    ", DOWNLOAD_DATE" + 		//  TIMESTAMP NULL,
+			    ", IMPORT_DATE" + 			//  TIMESTAMP NULL,
+			    ", SOURCESYSTEM_CD" + 		//  VARCHAR(50) NULL,
+			    ", UPLOAD_ID ) " + 			//  INT NULL,
+			"VALUES" +
+			   "( <PATIENT_IDE>" +
+			   ", <PATIENT_IDE_SOURCE>" +
+			   ", NULL" + 					// patient_ide_status        
+			   ", <PROJECT_ID>" +   
+			   ", now()" +
+			   ", now()" +
+			   ", now()" +
+			   ", now()" +
+			   ", <SOURCESYSTEM_CD>" +
+			   ", NULL ) ;" ;				// upload id
+	
+	private ProjectUtils utils ;
+	
+	private String schema_name = null ;
+	
+	private String patient_ide = null ;
+	private String patient_ide_source = null ;
+	private Integer patient_num = null ;
+	private String patient_ide_status = null ;
+	private String project_id = null ;
+	private String sourcesystem_id = null ;
+	
+	public PatientMapping( ProjectUtils utils ) {
+		this.utils = utils ;
+	}
+	
+	
+	public void serializeToDatabase( Connection connection ) throws UploaderException {
+		enterTrace( "PatientMapping.serializeToDatabase()" ) ;
+		try {
+			String sqlCmd = PATIENT_MAP_INSERT_COMMAND ;			
+			sqlCmd = sqlCmd.replaceAll( "<DB_SCHEMA_NAME>", schema_name ) ;			
+			sqlCmd = sqlCmd.replace( "<PATIENT_IDE>", utils.enfoldString( patient_ide ) ) ;
+			sqlCmd = sqlCmd.replace( "<PATIENT_IDE_SOURCE>", utils.enfoldString( patient_ide_source ) ) ;
+			sqlCmd = sqlCmd.replace( "<PROJECT_ID>", utils.enfoldString( project_id ) ) ;
+			sqlCmd = sqlCmd.replace( "<SOURCESYSTEM_CD>", utils.enfoldNullableString( sourcesystem_id ) ) ;			
+			Statement st = connection.createStatement();			
+			st.execute( sqlCmd ) ;
+			ResultSet rs = st.executeQuery( "select currval( 'PATIENT_MAPPING_PATIENT_NUM_seq');" ) ;
+			rs.next();
+			patient_num = rs.getInt(1) ;		
+			rs.close() ;
+		}
+		catch( SQLException sqlx ) {
+			throw new UploaderException( "Failed to insert into patient mapping.", sqlx ) ;
+		}
+		finally {
+			exitTrace( "PatientMapping.serializeToDatabase()" ) ;
+		}
+	}
+	
+	
+	public boolean mappingExists( Connection connection ) throws UploaderException {
+		enterTrace( "PatientMapping.mappingExists()" ) ;
+		boolean exists = false ;
+		try {
+			//
+			// See whether the appropriate patient mapping already exists in the db...
+			Statement st = connection.createStatement() ;
+			st.executeQuery( "select PATIENT_NUM from " + schema_name + ".PATIENT_MAPPING "  
+				           + " where " 
+				           + " PATIENT_IDE = '" + patient_ide + "' " 
+					       + "  and " 
+				           + " PATIENT_IDE_SOURCE = '" + patient_ide_source + "' " 
+					       + "  and "
+				           + " PROJECT_ID = '" + project_id + "' ;" ) ;			
+		    ResultSet rs = st.getResultSet() ;
+		    if( rs.next() ) {
+		    	patient_num = rs.getInt(1) ;			    
+			    exists = true ;
+				rs.close() ;
+		    }
+			return exists ;
+		}
+		catch( SQLException sqlx ) {
+			String message = "Failed to detect whether the appropriate patient mapping already exists in the db." ;
+			logger.error( message, sqlx ) ;
+			throw new UploaderException( message, sqlx ) ;
+		}
+		finally {
+			exitTrace( "PatientMapping.mappingExists()" ) ;
+		}		
+	}
+	
+	
+	/**
+	 * Utility routine to enter a structured message in the trace log that the given method 
+	 * has been entered. 
+	 * 
+	 * @param entry: the name of the method entered
+	 */
+	public static void enterTrace( String entry ) {
+		I2B2Project.enterTrace( logger, entry ) ;
+	}
+
+    /**
+     * Utility routine to enter a structured message in the trace log that the given method 
+	 * has been exited. 
+	 * 
+     * @param entry: the name of the method exited
+     */
+    public static void exitTrace( String entry ) {
+    	I2B2Project.exitTrace( logger, entry ) ;
+	}
+
+
+	public void setSchema_name(String schema_name) {
+		this.schema_name = schema_name;
+	}
+
+
+	public void setPatient_ide(String patient_ide) {
+		this.patient_ide = patient_ide;
+	}
+	
+	public String getPatient_ide() {
+		return this.patient_ide ;
+	}
+
+
+	public void setPatient_ide_source(String patient_ide_source) {
+		this.patient_ide_source = patient_ide_source;
+	}
+
+
+//	public void setPatient_num(Integer patient_num) {
+//		this.patient_num = patient_num;
+//	}
+	
+	public Integer getPatient_num() {
+		return this.patient_num ;
+	}
+
+
+	public void setPatient_ide_status(String patient_ide_status) {
+		this.patient_ide_status = patient_ide_status;
+	}
+
+
+	public void setProject_id(String project_id) {
+		this.project_id = project_id;
+	}
+
+
+	public void setSourcesystem_id(String sourcesystem_id) {
+		this.sourcesystem_id = sourcesystem_id;
+	}
+}
